ffbf10bc2ed4526d3e0fc1bf2fc8b8b42b04669f
Brian Barros <brian.ferreira@pie.tax>
2024-04-26 02:00:03 -0300

    Rate limit configuration for web and api routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +33,37 @@ class AppServiceProvider extends ServiceProvider
 
         /** DB config */
         Schema::defaultStringLength(191);
+
+        /** Rate limit config */
+        $this->configRateLimiterForWeb();
+        $this->configRateLimiterForApi();
+    }
+
+    /**
+     * Configuring the maximum number of requests per minute for the `web` route group
+     *
+     * @return void
+     */
+    private function configRateLimiterForWeb(): void
+    {
+        RateLimiter::for('web', function (Request $request) {
+            return $request->user()
+                ? Limit::perMinute(200)->by($request->user()->getKey())
+                : Limit::perMinute(100)->by($request->ip());
+        });
+    }
+
+    /**
+     * Configuring the maximum number of requests per minute for the `api` route group
+     *
+     * @return void
+     */
+    private function configRateLimiterForApi(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return $request->user()
+                ? Limit::perMinute(200)->by($request->user()->getKey())
+                : Limit::perMinute(100)->by($request->ip());
+        });
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->throttleWithRedis();
+        $middleware->web('throttle:web');
+        $middleware->api('throttle:api');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
