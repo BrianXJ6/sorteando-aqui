a533153154d2deea2909c3fad86b37173cf6535c
Brian Barros <brian.ferreira@pie.tax>
2024-04-28 02:58:57 -0300

    new field for user to store image (avatar)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use App\Support\ORM\BaseAuthenticable;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends BaseAuthenticable implements MustVerifyEmail
 {
@@ -15,6 +17,7 @@ class User extends BaseAuthenticable implements MustVerifyEmail
     protected $fillable = [
         'name',
         'email',
+        'avatar',
         'password',
     ];
 
@@ -38,5 +41,32 @@ class User extends BaseAuthenticable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Accessors and Mutators
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Get the user's avatar image.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: function (?string $path) {
+                if (!$path) {
+                    return null;
+                }
+
+                /** @var \Illuminate\Filesystem\Filesystem */
+                $disk = Storage::disk('s3-user-avatar');
+
+                return $disk->url($path);
+            },
+        );
     }
 }
