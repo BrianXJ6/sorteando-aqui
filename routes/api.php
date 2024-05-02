faeef5abdaf6287cf8632d68f6a2ba8e7e9dfe2e
Brian Barros <brian.ferreira@pie.tax>
2024-05-02 15:09:07 -0300

    Defining a name for api routes

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Auth routes
-|--------------------------------------------------------------------------
-*/
-Route::prefix('auth')->name('auth.')->group(function () {
-    // Users routes
-    Route::prefix('users')->name('users.')->controller(UserAuthController::class)->group(function () {
-        // Guest
-        Route::middleware(['guest:sanctum'])->group(function () {
-            Route::post('signin', 'signInApi')->name('signin-api');
-        });
-        // Authenticated
-        Route::middleware(['auth:sanctum'])->group(function () {
-            Route::post('signout', 'signOut')->name('signout-api');
+Route::name('api.')->group(function () {
+    /*
+    |--------------------------------------------------------------------------
+    | Auth routes
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('auth')->name('auth.')->group(function () {
+        // Users routes
+        Route::prefix('users')->name('users.')->controller(UserAuthController::class)->group(function () {
+            // Guest
+            Route::middleware(['guest:sanctum'])->group(function () {
+                Route::post('signin', 'signInApi')->name('signin-api');
+            });
+            // Authenticated
+            Route::middleware(['auth:sanctum'])->group(function () {
+                Route::post('signout', 'signOut')->name('signout-api');
+            });
         });
     });
 });
