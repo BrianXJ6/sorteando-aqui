f1ec48cb89e2ce4f7d08ed1c04b47b26ac99c68a
Brian Barros <brian.ferreira@pie.tax>
2024-05-07 16:32:13 -0300

    route home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
+Route::get('/', fn () => 'Home Page')->name('home');
+
 /*
 |--------------------------------------------------------------------------
 | Action routes
